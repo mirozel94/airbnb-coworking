47fa8894bcdacc84be8953f7a885c09fc2d21e3e
David Michel <davidmichel77@gmail.com>
2017-08-17 14:30:12 +0200

    added an ugly book form and a favicon

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,16 @@ class BookingsController < ApplicationController
   def index
     @bookings = current_user.bookings
   end
+
+  def create
+    @booking = Booking.new
+    @booking = Booking.find(params[:booking_id])
+
+
+  end
+
+  def review_params
+    params.require(:booking).permit(:content)
+  end
+
 end
