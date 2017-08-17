52c965f689d735f06ab92a77d415081edb7d5216
Jean Daniel DULOIR <jdduloir@icloud.com>
2017-08-17 12:18:56 +0200

    pop up de validation

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -20,7 +20,7 @@ class SpacesController < ApplicationController
     @space = Space.new(space_params)
     @space.user = current_user
     if @space.save
-      redirect_to space_path(@space)
+      redirect_to space_path(@space), notice: "your space have been created"
     else
       render :new
     end
