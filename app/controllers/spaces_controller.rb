f06babe17cff7d129fee354cdf9eaa774090b0b0
David Michel <davidmichel77@gmail.com>
2017-08-18 13:06:55 +0200

    save chgs

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,7 +1,6 @@
 class SpacesController < ApplicationController
 
   def index
-    binding.pry
     @spaces = Space.where.not(latitude: nil, longitude: nil)
     @hash = Gmaps4rails.build_markers(@spaces) do |space, marker|
       marker.lat space.latitude
