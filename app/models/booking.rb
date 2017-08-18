67b5f8387b73d0b2e7fe20816f0bcc1bc3cf0b9e
David Michel <davidmichel77@gmail.com>
2017-08-18 15:41:51 +0200

    save state

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :space
+
+  def self.compute_price(daterange, people, space_price)
+    dates = daterange.split(" - ")
+    duration = Date.strptime(dates[1], "%m/%d/%Y") - Date.strptime(dates[0], "%m/%d/%Y") + 1
+    price = (space_price.fdiv(30)) * duration * people.to_i
+  end
+
 end
